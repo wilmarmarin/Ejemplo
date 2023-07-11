71a01f7d17877e1b91b32f51933eb0b250886677
Juctorolo <87885312+Juctorolo@users.noreply.github.com>
2023-07-11 10:51:37 -0500

    11/07_2

--- Ejemplo/Assets/OArielG/Scripts/QuitManager.cs
+++ Ejemplo/Assets/OArielG/Scripts/QuitManager.cs
@@ -6,7 +6,7 @@ public class QuitManager: MonoBehaviour
 {
     public void Exit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 }
--- Ejemplo/Assets/OArielG/Scripts/UISpriteAnimation.cs
+++ Ejemplo/Assets/OArielG/Scripts/UISpriteAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.WSA;
+//using UnityEngine.WSA;
 
 public class UISpriteAnimation: MonoBehaviour
 {
